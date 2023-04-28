e4509af5612c8fa4d46827deaa2b699ce489604d
Alexander-Wilhelm-Student <alwid46@student.oaisd.org>
2023-04-28 14:37:35 -0400

    morbius tower, yippee script, TODO add yippee explosion
    fortnite

---
+++ Assets/Scripts/SpawnAfterTime.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAfterTime : MonoBehaviour
+{
+    AudioPlayer audioPlayer;
+    [SerializeField] GameObject spawnObject;
+    [SerializeField] AudioClip spawnSound;
+    [SerializeField] float soundVolume;
+    [SerializeField] float SpawnDelay;
+    [SerializeField] float SpawnDelayVariance;
+    [SerializeField] float postSoundDelay;
+    [SerializeField] bool destroyOnSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+        float waitTime = SpawnDelay + Random.Range(0, SpawnDelayVariance);
+        Invoke("Sound", waitTime);
+    }
+
+    void Sound() {
+        audioPlayer.PlayClip(spawnSound, soundVolume);
+        Invoke("Spawn", postSoundDelay);
+    }
+
+    void Spawn() {
+        Instantiate(spawnObject, transform.position, Quaternion.identity);
+        if (destroyOnSpawn) Destroy(gameObject);
+    }
+}
